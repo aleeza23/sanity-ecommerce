5947d7cce392f48b907d1562ecab6b07419f9975
Aleeza Rubab <malikaleeza886@gmail.com>
2025-04-05 09:45:14 -0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,9 @@ import Categories from "@/features/Categories";
 import HeroSection from "@/features/Hero";
 import { Navbar } from "@/features/Navbar";
 
+export const revalidate = 30
 export default function Home() {
+
   return (
     <>
       <Navbar />
